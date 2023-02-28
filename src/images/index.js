6e15e0a802144f21e3d6cf9a1bcb0b2d5e7a5c03
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-02-28 16:17:01 +0300

    Add svg images

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,8 @@
 import UsersIcon from './users.svg';
 import DashboardIcon from './dashboard.svg';
 import ProfileIcon from './profile.svg';
+import BackburgerIcon from './backburger.svg';
+import MenuIcon from './menu.svg';
+import CloseIcon from './close.svg';
 
-export { UsersIcon, DashboardIcon, ProfileIcon };
+export { CloseIcon, MenuIcon, BackburgerIcon, UsersIcon, DashboardIcon, ProfileIcon };
