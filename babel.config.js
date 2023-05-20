71dda27223579483e4437d474d7c73837e320619
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-05-20 19:15:46 +0300

    Modify common aliases config & append testing aliases

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         root: '.',
         alias: {
           '@images': './src/images',
+          '@environments': './src/environments',
           '@constants': './src/constants',
           '@helpers': './src/helpers',
           '@navigation': './src/navigation',
@@ -15,6 +16,12 @@ module.exports = {
           '@stores': './src/stores',
           '@context': './src/context'
         }
+      }
+    ],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true
       }
     ],
     ['react-native-reanimated/plugin']
